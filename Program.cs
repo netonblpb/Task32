09867a3f7b5eb93a544108da72d79caf78c8a9c6
netonblpb <skullbog@gmail.com>
2022-02-11 16:01:30 +0300

    Task32 complete

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте массив из восьми элементов, заполненных нулями и единицами. Выведите их на экран
+
+Console.Clear();
+int[] array = new int[8];
+Random random = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(0, 2);
+    Console.Write(array[i] + "     ");
+}
